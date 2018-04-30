12e2d3d86b947c7e6767aca8c2545b1e27011bcb
collinsn@corp <nate.collins@espn.com>
2018-04-30 16:47:12 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,19 @@
-name             "virtualbox"
-maintainer       "Bradley D Smith"
-maintainer_email "bradleydsmith@gmail.com"
-license          "Apache 2.0"
-description      "Installs virtualbox"
+name 'virtualbox'
+issues_url 'https://github.com/steveballmer/virtualbox-cookbook/issues'
+source_url 'https://github.com/steveballmer/virtualbox-cookbook'
+chef_version '>= 13.8.5' if respond_to?(:chef_version)
+maintainer 'Steve Ballmer'
+maintainer_email 'bradleydsmith@gmail.com'
+license 'Apache 2.0'
+description 'Installs virtualbox'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.4"
+version '1.1.0'
 
-%w{ubuntu debian centos redhat mac_os_x windows fedora}.each do |os|
-  supports os
-end
+supports 'ubuntu', '= 14.04'
+supports 'centos', '= 7.2'
 
-depends "dmg"
-depends "windows"
-depends "apt"
-depends "yum"
-depends "apache2"
+depends 'dmg'
+depends 'windows'
+depends 'apt'
+depends 'yum'
+depends 'apache2'
